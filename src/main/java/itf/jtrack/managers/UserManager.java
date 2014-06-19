895b80a02183e86e2e50d3bdcc65ee1ff0ab089b
Martin Welß <mw@it-focus.de>
2014-06-19 17:31:01 +0200

    restful json webservices

--- src/main/java/itf/jtrack/managers/UserManager.java
+++ src/main/java/itf/jtrack/managers/UserManager.java
@@ -8,23 +8,38 @@ import javax.ejb.Stateless;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 @Stateless
 @Named(value="usermanager")
+@Path("user")
 public class UserManager {
 
 	@PersistenceContext EntityManager em;
 	
+	@POST
+	@Consumes("application/json")
+	@Produces("application/json")
 	public User save(User user) {
 		log("saving user: "+user);
 		return em.merge(user);
 	}
 
+	@GET
+	@Produces("application/json")
 	public List<User> findAll() {
 		return em.createQuery("select u from users u", User.class).getResultList();
 	}
 	
-	public User find(long userid) {
+	@GET
+	@Path("{id}")
+	@Produces("application/json")
+	public User find(@PathParam("id") long userid) {
 		return em.find(User.class, userid);
 	}
 	
---
+++ src/main/java/itf/jtrack/web/RestApplication.java
@@ -0,0 +1,10 @@
+package itf.jtrack.web;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("items")
+public class RestApplication extends Application {
+	
+
+}
