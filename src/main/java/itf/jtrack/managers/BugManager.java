54d4c263dde00ebbd96d1bc8a14ad7b7f9749074
Martin Welß <mw@it-focus.de>
2014-07-01 14:05:34 +0200

    webservices for all managers

--- src/main/java/itf/jtrack/managers/BugManager.java
+++ src/main/java/itf/jtrack/managers/BugManager.java
@@ -10,13 +10,22 @@ import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Stateless
 @Named(value="bugmanager")
+@Path("bug")
 public class BugManager {
 
 	@PersistenceContext EntityManager em;
 	
+	@POST
+	@Consumes("application/json")
+	@Produces("application/json")
 	public Bug save(Bug bug) {
 		log("saving bug: "+bug);
 		if(bug.getBugid()==null) {
@@ -26,10 +35,14 @@ public class BugManager {
 		return em.merge(bug);
 	}
 	
+	@GET
+	@Produces("application/json")	
 	public List<Bug> getRecentlyChangedBugs() {
 		return em.createQuery("select b from bugs b order by b.lastchange desc", Bug.class).setMaxResults(10).getResultList();
 	}
 	
+	@GET
+	@Produces("application/json")	
 	public Bug find(long bugid) {
 		return em.find(Bug.class, bugid);
 	}
@@ -39,6 +52,9 @@ public class BugManager {
 	}
 
 	@SuppressWarnings("unchecked")
+	@POST
+	@Consumes("application/json")
+	@Produces("application/json")
 	public List<Bug> search(String text) {
 		text="%"+text.toLowerCase().trim()+"%";
 		Query query=em.createQuery("select b from bugs b where b.title like :text1 or b.description like :text2", Bug.class);
--- src/main/java/itf/jtrack/managers/ProductManager.java
+++ src/main/java/itf/jtrack/managers/ProductManager.java
@@ -8,22 +8,35 @@ import javax.ejb.Stateless;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Stateless
 @Named(value="productmanager")
+@Path("product")
 public class ProductManager {
 
 	@PersistenceContext EntityManager em;
 	
+	@POST
+	@Consumes("application/json")
+	@Produces("application/json")
 	public Product save(Product prod) {
 		log("saving product: "+prod);
 		return em.merge(prod);
 	}
 
+	@GET
+	@Produces("application/json")
 	public List<Product> findAll() {
 		return em.createQuery("select p from products p", Product.class).getResultList();
 	}
 	
+	@GET
+	@Produces("application/json")
 	public Product find(long productid) {
 		return em.find(Product.class, productid);
 	}
